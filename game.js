5fcc804c21cb1a447320e16b8c35d9c3fe6f3dca
Yannis <yannismadu777@gmail.com>
2023-07-01 00:57:42 +0100

    Add flex wrap and correct width to grid boxes

--- game.js
+++ game.js
@@ -12,20 +12,20 @@ function generateGrid(gridSize) {
 
     // Create divs for grid
     for (let singleDiv = totalDivs; singleDiv > 0; singleDiv--) {
+        const gridContainer = document.querySelector(".gridContainer");
         const box = document.createElement("div");
-        let gridContainer = document.querySelector(".gridContainer");
+        box.classList.add("box");
+        box.style.width = `${divWidth}px`;
         gridContainer.appendChild(box);
-        console.log(totalDivs);
     };
 };
-
-
 
 function getGridSize() {
     // Grid slider and value output
     const slider = document.querySelector(".gridSizeSlider");
     const value = document.querySelector(".gridSizeValue");
     value.textContent = slider.value;
+    generateGrid(slider.value);
     slider.oninput = () => {
         refreshGrid()
         value.textContent = slider.value;
