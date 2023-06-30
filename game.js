ef6ebd68870729c4c217af5393926078c3d3c3fb
Yannis <yannismadu777@gmail.com>
2023-07-01 00:06:52 +0100

    Add generate divs functionality
    Need to add a function to allow the generateGrid function
    to destroy old divs and recreate the grid when the user
    changes the grid size

--- game.js
+++ game.js
@@ -1,15 +1,22 @@
-function getGridSize() {
+function generateGrid() {
+    // Grid slider and value output
     const slider = document.querySelector(".gridSizeSlider");
     const value = document.querySelector(".gridSizeValue");
     value.textContent = slider.value;
     slider.oninput = () => {
         value.textContent = slider.value;
-    }
+    };
+
+    // Grid width calculations
+    totalDivs = slider.value**2;
+    divWidth = 500/slider.value;
+
+    // Create divs for grid
+    for (let singleDiv = totalDivs; singleDiv > 0; singleDiv--) {
+        const box = document.createElement("div");
+        let gridContainer = document.querySelector(".gridContainer");
+        gridContainer.appendChild(box);
+    };
 };
 
-
-function generateGrid() {
-
-};
-
-getGridSize()
+generateGrid()
