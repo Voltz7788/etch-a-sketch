66a8b04fe0777a7bfd022e97a9d9455bb11e3991
Yannis <yannismadu777@gmail.com>
2023-07-02 20:28:46 +0100

    Added grid color change on mouse hover

--- game.js
+++ game.js
@@ -1,3 +1,52 @@
+function createColorPicker() {
+    const pickr = Pickr.create({
+        el: '.color-picker',
+        theme: 'nano', // or 'monolith', or 'nano'
+    
+        swatches: [
+            'rgba(244, 67, 54, 1)',
+            'rgba(233, 30, 99, 0.95)',
+            'rgba(156, 39, 176, 0.9)',
+            'rgba(103, 58, 183, 0.85)',
+            'rgba(63, 81, 181, 0.8)',
+            'rgba(33, 150, 243, 0.75)',
+            'rgba(3, 169, 244, 0.7)',
+            'rgba(0, 188, 212, 0.7)',
+            'rgba(0, 150, 136, 0.75)',
+            'rgba(76, 175, 80, 0.8)',
+            'rgba(139, 195, 74, 0.85)',
+            'rgba(205, 220, 57, 0.9)',
+            'rgba(255, 235, 59, 0.95)',
+            'rgba(255, 193, 7, 1)'
+        ],
+    
+        components: {
+    
+            // Main components
+            preview: true,
+            opacity: true,
+            hue: true,
+    
+            // Input / output Options
+            interaction: {
+                hex: true,
+                rgba: false,
+                hsla: false,
+                hsva: false,
+                cmyk: false,
+                input: true,
+                clear: true,
+                save: true
+            }
+        }
+    });
+};
+
+function addColor(box) {
+};
+
+
+
 function refreshGrid() {
     const gridContainer = document.querySelector(".gridContainer");
     while(gridContainer.firstChild) {
@@ -16,19 +65,26 @@ function generateGrid(gridSize) {
         const box = document.createElement("div");
         box.classList.add("box");
         box.style.width = `${divWidth}px`;
+        box.addEventListener("mouseenter", (event) => {
+            box.style.backgroundColor = "#333333";
+        });
+
+
+
         gridContainer.appendChild(box);
     };
 };
 
 function getGridSize() {
     // Grid slider and value output
+    createColorPicker()
     const slider = document.querySelector(".gridSizeSlider");
     const value = document.querySelector(".gridSizeValue");
-    value.textContent = slider.value;
+    value.textContent = `${slider.value} x ${slider.value}`;
     generateGrid(slider.value);
     slider.oninput = () => {
         refreshGrid()
-        value.textContent = slider.value;
+        value.textContent = `${slider.value} x ${slider.value}`;
         generateGrid(slider.value);
     };
 };
